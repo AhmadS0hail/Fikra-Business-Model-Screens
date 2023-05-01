2820fb96a938cdc19e6f45c0eb232246f41509b9
akmail2 <ahmad.it.2008@gmail.com>
2023-05-01 12:04:51 +0300

    fikra wordpress and vue template

---
+++ wp-content/themes/fikra/includes/templates/functions.php
@@ -0,0 +1,303 @@
+<?php
+function modify_templates_init()
+{
+    register_post_type('templates', array(
+        'label' => 'Templates',
+        'labels' => array(
+            'name' => 'Templates  ',
+            'singular_name' => 'Templates  ',
+            'menu_name' => 'Templates  ',
+            'name_admin_bar' => 'Templates  ',
+        ),
+        'description' => '',
+        'public' => false,
+        'publicly_queryable' => false,
+        'query_var' => false,
+        'rewrite' => false,
+        'has_archive' => false,
+        'hierarchical' => false,
+        'exclude_from_search' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => false,
+        'menu_icon' => 'dashicons-media-document',
+        'supports' => array('title', 'excerpt', 'thumbnail','page-attributes'),
+        'can_export' => true,
+    ));
+
+    // Add new taxonomy, NOT hierarchical (like tags)
+    $labels = array(
+        'name'                       => 'Keywords',
+        'singular_name'              => 'Keyword',
+        'all_items'                  => 'Keywords',
+        'parent_item'                => null,
+        'menu_name'                  => 'Keywords',
+    );
+
+    $args = array(
+        'hierarchical'          => false,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_admin_column'     => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'Keywords' ),
+    );
+
+    register_taxonomy( 'Keywords', ['templates','question'], $args );
+
+}
+
+add_action('init', 'modify_templates_init');
+/********
+ *
+ * Meta Boxes
+ *
+ *******/
+add_action('add_meta_boxes', 'templates_meta_box');
+function templates_meta_box()
+{
+    add_meta_box(
+        'templates_meta_id',
+        'Information',
+        'modify_function_templates',
+        'templates'
+    );
+}
+
+function modify_function_templates()
+{
+    global $post;
+//    $keywords = get_post_meta($post->ID, 'keywords');
+    $title_en = get_post_meta($post->ID, 'title_en', true);
+    $file_ar = get_post_meta($post->ID, 'file_ar', true);
+    $file_en = get_post_meta($post->ID, 'file_en', true);
+    ?>
+    <ul class="_list">
+        <li><label><span><b> Title (English) : </b></span></label>
+            <input type="text" name="title_en" value="<?php echo $title_en; ?>">
+
+        </li>
+
+
+        <li>
+
+            <label><span>ملف النموذج 1</span></label>
+
+            <input type="text" name="file_ar" id="file_ar" size="25" value="<?php echo $file_ar; ?>">
+
+            <button data-inputid="file_ar" type="button"
+
+                    class="button  upload_img"> الملف 1
+
+            </button>
+
+
+        </li>
+        <li>
+
+            <label><span>ملف النموذج 2</span></label>
+
+            <input type="text" name="file_en" id="file_en" size="25" value="<?php echo $file_en; ?>">
+
+            <button data-inputid="file_en" type="button"
+
+                    class="button  upload_img2"> الملف 2
+
+            </button>
+
+
+        </li>
+
+
+    </ul>
+    <?php
+    /*
+    <h3>
+        <b><u>Keywords:</u></b>
+    </h3>
+    <ul class="_list keywords">
+        <?php
+        if ($keywords) {
+            foreach ($keywords as $k => $keyword) { ?>
+                <li><label><span><b> Keyword <?php echo ++$k; ?> : </b></span></label>
+                    <input type="text" name="keyword[]" value="<?php echo $keyword; ?>">
+                    <button class="button delete-this" type="button">Delete</button>
+                </li>
+            <?php }
+        } ?>
+    </ul>
+    <button class="add-new-Keyword button" type="button">+ Add new Keyword</button>
+   */?>
+    <script>
+        // jQuery("body").on('click', '.add-new-Keyword', function () {
+        //     let c = jQuery(".keywords li").length + 1;
+        //     let h = `<li><label><span><b> Keyword ` + c + ` : </b></span></label>
+        //             <input type="text" name="keyword[]">
+        //             <button class="button delete-this" type="button">Delete</button>
+        //         </li>`;
+        //     jQuery(".keywords").append(h);
+        // });
+        // jQuery("body").on('click', '.delete-this', function () {
+        //     jQuery(this).closest('li').remove();
+        // });
+
+        jQuery(document).ready(function ($) {
+            var file_frame, file_frame2;
+            jQuery('body').on('click', '.upload_img', function (event) {
+
+                var inputid = jQuery(this).attr('data-inputid');
+                event.preventDefault();
+                // If the media frame already exists, reopen it.
+                if (file_frame) {
+                    file_frame.open();
+                    return;
+                }
+                // Create the media frame.
+                file_frame = wp.media.frames.file_frame = wp.media({
+                    title: jQuery(this).data('uploader_title'),
+                    button: {
+                        text: jQuery(this).data('uploader_button_text'),
+                    },
+                    multiple: false  // Set to true to allow multiple files to be selected
+                });
+                // When an image is selected, run a callback.
+                file_frame.on('select', function () {
+                    // We set multiple to false so only get one image from the uploader
+                    attachment = file_frame.state().get('selection').first().toJSON();
+                    jQuery("#" + inputid).val(attachment.url);
+                    // Do something with attachment.id and/or attachment.url here
+                });
+                // Finally, open the modal
+                file_frame.open();
+            });
+            jQuery('body').on('click', '.upload_img2', function (event) {
+
+
+                var imgid = jQuery(this).attr('data-imgid');
+
+
+                var inputid = jQuery(this).attr('data-inputid');
+
+
+                event.preventDefault();
+
+
+                // If the media frame already exists, reopen it.
+
+
+                if (file_frame2) {
+
+
+                    file_frame2.open();
+
+
+                    return;
+
+
+                }
+
+
+                // Create the media frame.
+
+
+                file_frame2 = wp.media.frames.file_frame2 = wp.media({
+
+
+                    title: jQuery(this).data('uploader_title'),
+
+
+                    button: {
+
+
+                        text: jQuery(this).data('uploader_button_text'),
+
+
+                    },
+
+
+                    multiple: false  // Set to true to allow multiple files to be selected
+
+
+                });
+
+
+                // When an image is selected, run a callback.
+
+
+                file_frame2.on('select', function () {
+
+
+                    // We set multiple to false so only get one image from the uploader
+
+
+                    attachment = file_frame2.state().get('selection').first().toJSON();
+
+
+                    // var p = jQuery(this).parent();
+
+
+                    // console.log(p.html());
+
+
+                    // p.find("img").attr("src",attachment.url);
+
+
+                    // p.find("img").removeClass("uk-hidden");
+
+
+                    // p.find("input[type=hidden]").val(attachment.url);
+
+
+                    console.log(imgid);
+
+
+                    jQuery("#" + imgid).attr("src", attachment.url);
+
+
+                    jQuery("#" + imgid).show();
+
+
+                    jQuery("#" + inputid).val(attachment.url);
+
+
+                    // Do something with attachment.id and/or attachment.url here
+
+
+                });
+
+
+                // Finally, open the modal
+
+
+                file_frame2.open();
+
+
+            });
+
+        });
+    </script>
+
+    <?php
+
+
+}
+
+function save_templates_meta($post_id = false, $post = false)
+{
+    // Check post type for teachers
+    if ($post->post_type == 'templates') {
+        update_post_meta($post_id, 'title_en', $_POST['title_en']);
+        update_post_meta($post_id, 'file_ar', $_POST['file_ar']);
+        update_post_meta($post_id, 'file_en', $_POST['file_en']);
+
+//        delete_post_meta($post_id, 'keywords');
+//        foreach ($_POST['keyword'] as $k => $answer) {
+//            add_post_meta($post_id, 'keywords', $answer);
+//        }
+    }
+}
+
+add_action('save_post', 'save_templates_meta', 10, 2);
+
---
+++ wp-content/themes/fikra/src/components/FormPage/FromOne.vue
@@ -0,0 +1,226 @@
+<template>
+	<form @submit="onSubmit">
+		<div class="mb-20 space-y-4 md:space-y-6">
+			<h2 class="text-[#1C1C1C] text-xl font-bold">المعلومات الشخصية</h2>
+			<!-- Name -->
+			<BaseInput
+				type="text"
+				name="name"
+				label="الاسم الثلاثي *"
+				placeholder="يرجى كتابة الاسم هنا"
+				v-model="name"
+				:icon="true"
+				:errorState="errors.name">
+			</BaseInput>
+			<!-- Phone Number -->
+			<div>
+				<label class="block mb-2 text-sm font-medium text-grey">رقم الجوال *</label>
+				<vue-tel-input
+					dir="ltr"
+					:class="{ isError: invalidPhoneNumber }"
+					v-model="phone"
+					v-bind="phoneOptions"
+					@on-input="checkPhone">
+					<template #icon-right> <img :src="_settings.tlink+'/src/assets/img/phone.svg'" alt="Icon" class="w-5" /></template>
+					<template #arrow-icon> <span class="icon arrow-downward"></span></template>
+				</vue-tel-input>
+				<p v-if="invalidPhoneNumber" class="block mt-2 text-sm font-medium text-red-500">يجب أن يكون رقم الجوال 9 أرقام</p>
+			</div>
+			<!-- Email -->
+			<BaseInput
+				type="email"
+				name="email"
+				label="البريد الالكتروني *"
+				placeholder="اكتب البريد الالكتروني هنا"
+				v-model="email"
+				:errorState="errors.email">
+			</BaseInput>
+		</div>
+		<div class="mb-8 space-y-4 md:space-y-6">
+			<h2 class="text-[#1C1C1C] text-xl font-bold">معلومات المشروع</h2>
+			<!-- Project Name -->
+			<BaseInput
+				type="text"
+				name="project"
+				label="اسم المشروع *"
+				placeholder="اكتب اسم المشروع هنا"
+				v-model="project"
+				:errorState="errors.project">
+			</BaseInput>
+			<!-- Project Type Option -->
+			<div>
+				<label class="block mb-2 text-sm font-medium text-grey">مجال المشروع *</label>
+				<vue-select
+					class="bg-white border border-gray-300 text-grey sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full py-1.5 px-2.5 outline-none"
+					:class="{ isError: unSelectedprojectDomain }"
+					:multiple="false"
+					v-model="projectDomain"
+					:options="projectDomains"
+					:placeholder="'يرجى الاختيـار'"
+					:close-on-select="true"
+					@selected="toggleSelectErrorState">
+				</vue-select>
+				<p v-if="unSelectedprojectDomain" class="block mt-2 text-sm font-medium text-red-500">هذا الحقل مطلوب</p>
+			</div>
+		</div>
+		<button type="submit" class="w-full py-2 mt-6 text-center text-white rounded-full bg-primary">التالي</button>
+	</form>
+</template>
+
+<script setup>
+	import BaseInput from "../Base/Input.vue";
+	import { projectDomains } from "../../utils/formQuestions";
+	import { useField, useForm } from "vee-validate";
+	import { firstFormSchema } from "../../utils/zodSchema";
+	import { ref } from "vue";
+
+	const emit = defineEmits(["validSubmission"]);
+
+	const { handleSubmit, errors } = useForm({
+		validationSchema: firstFormSchema,
+	});
+
+	const { value: name } = useField("name");
+	const { value: email } = useField("email");
+	const { value: project } = useField("project");
+
+	// Phone Related Login Initialization
+	const phone = ref(null);
+	const phoneOptions = {
+		mode: "international",
+		defaultCountry: "SA",
+		// Hide Israel From Counrty Options
+		ignoredCountries: ["IL"],
+		autoDefaultCountry: false,
+		validCharactersOnly: true,
+		autoFormat: true,
+		inputOptions: {
+			autocomplete: "off",
+			maxlength: 25,
+			placeholder: "000000000",
+		},
+		dropdownOptions: {
+			showFlags: true,
+			showDialCodeInSelection: true,
+		},
+	};
+	const invalidPhoneNumber = ref(false);
+
+	// Project Type Related Login Initialization
+	const projectDomain = ref(null);
+	const unSelectedprojectDomain = ref(false);
+
+	// Change the selection error state
+	function toggleSelectErrorState() {
+		unSelectedprojectDomain.value = false;
+	}
+
+	// Validate Phone Number (Currently Only For Saudi Arabia)
+	function checkPhone(num) {
+		if (num) {
+			const pattern = /^(?:(?:\+|00)966)?5\d{8}$/;
+			let trimmed = num.replace(/\s+/g, "").trim();
+			if (!pattern.test(trimmed)) {
+				invalidPhoneNumber.value = true;
+			} else {
+				invalidPhoneNumber.value = false;
+			}
+		}
+	}
+
+	// Handle Invalid Form Submission (Check for Invalid Phone or Unselected Project Type)
+	function onInvalidSubmit() {
+		if (!projectDomain.value) {
+			unSelectedprojectDomain.value = true;
+		}
+
+		if (!phone.value) {
+			invalidPhoneNumber.value = true;
+		}
+	}
+
+	// Handle Submission
+	const onSubmit = handleSubmit((values) => {
+		// console.log("FORM ONE SUBMITTED");
+
+		// Check Project Type Selection
+		if (!projectDomain.value) {
+			unSelectedprojectDomain.value = true;
+			return;
+		}
+
+		// Check Phone Number Entry
+		if (!phone.value || invalidPhoneNumber.value) {
+			invalidPhoneNumber.value = true;
+			return;
+		}
+
+		unSelectedprojectDomain.value = false;
+		values.phone = phone.value;
+		values.projectDomain = projectDomain.value;
+
+		emit("validSubmission", "formOne", values);
+	}, onInvalidSubmit);
+</script>
+
+<style>
+	.isError {
+		border: 1px solid #fda29b !important;
+		box-shadow: #fee4e2 0px 1px 0px, #fee4e2 0px 0px 8px !important;
+	}
+
+	.vue-tel-input {
+		background-color: white !important;
+		border: 1px solid rgb(209, 213, 219) !important;
+		color: rgb(30, 30, 30) !important;
+		border-radius: 8px !important;
+		box-shadow: none !important;
+		padding-right: 10px !important;
+	}
+
+	.vti__input {
+		border-radius: 8px !important;
+		padding: 8px !important;
+	}
+
+	.vti__dropdown {
+		border-radius: 8px !important;
+		width: 128px !important;
+	}
+
+	.vue-tel-input:has(.vti__input:focus) {
+		border: 1px solid rgb(42, 100, 144) !important;
+	}
+
+	.vue-tel-input:has(.vti__dropdown.open) {
+		border: 1px solid rgb(42, 100, 144) !important;
+		border-radius: 8px !important;
+	}
+
+	.vue-tel-input.isError,
+	.vue-tel-input.isError:has(.vti__input:focus) {
+		border: 1px solid #fda29b !important;
+		box-shadow: #fee4e2 0px 1px 0px, #fee4e2 0px 0px 8px !important;
+	}
+
+	.vti__dropdown-list {
+		width: 300px;
+	}
+
+	.icon.arrow-downward {
+		box-sizing: border-box;
+		height: 10px;
+		width: 10px;
+		margin-left: 12px;
+		margin-bottom: 5px;
+		border-style: solid;
+		border-color: black;
+		border-width: 0px 1px 1px 0px;
+		transform: rotate(45deg);
+		transition: border-width 150ms ease-in-out;
+	}
+
+	.icon.arrow-downward.active {
+		transform: rotate(45deg);
+	}
+</style>
